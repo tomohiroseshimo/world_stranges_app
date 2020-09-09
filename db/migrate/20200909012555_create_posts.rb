4294a6f0b6889aeeb0ffcff65e9cd92d62f3365d
tomohiroseshimo <oresenyouzaku2@gmail.com>
2020-09-09 11:00:01 +0900

    マイグレーションファイルの記述
    edit migration file.

--- db/migrate/20200909012555_create_posts.rb
+++ db/migrate/20200909012555_create_posts.rb
@@ -1,7 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.string :title,   null: false 
+      t.string :images
+      t.body :text,   null: false
+      t.area :string,   null: false
+      t.level :string
+      t.references :user, foreign_key: true,  null: false
+      t.references :category, foreign_key: true,  null: false
       t.timestamps
     end
   end
