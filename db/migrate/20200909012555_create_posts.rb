7b3da5db9aa7605bd9a622b68e46edddea068f66
tomohiroseshimo <oresenyouzaku2@gmail.com>
2020-09-14 22:23:41 +0900

    postモデルのviewとcontrollerの作成
    rails g controller posts on termianl.
    create view file of 'index.html.haml'.
    and edit routes.rb

--- db/migrate/20200909012555_create_posts.rb
+++ db/migrate/20200909012555_create_posts.rb
@@ -3,11 +3,9 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.string :title,   null: false 
       t.string :images
-      t.body :text,   null: false
-      t.area :string,   null: false
-      t.level :string
-      t.references :user, foreign_key: true,  null: false
-      t.references :category, foreign_key: true,  null: false
+      t.text :body,   null: false
+      t.string :area,   null: false
+      t.string :level
       t.timestamps
     end
   end
